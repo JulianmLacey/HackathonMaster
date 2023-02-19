119e3e8599a32b4f98385fbe67d4855e8fb269f0
JulianmLacey <117090683+JulianmLacey@users.noreply.github.com>
2023-02-19 09:13:39 -0800

    Create Insert.sql

---
+++ Insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO userProfile
+(UserId,LastName,FirstName,walletId,nftId,eth,nftDate,Email,userName,UserPassword,r,g,b)
+VAlUES(657485,'Langer','Frank',5454544,7575757,6000,'2/16/21','pepper@mail.com','frankg','pepper',55,33,22),
+(567876,'Domgo','Philppe',6753456,7654356,8000,'2/20/23','georgreo@aol.com','itsphil','yup',45,60,21),
+(987678,'Franke','Geogre',9867564,5674856,500,'2/4/10','Happyman@gmail.com','Geopoo','heyeh',45,23,23);
