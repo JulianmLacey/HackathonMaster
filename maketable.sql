8c031ce4b218b0f5fe975b8963ab8fe9c1ffeeea
JulianmLacey <117090683+JulianmLacey@users.noreply.github.com>
2023-02-19 09:13:12 -0800

    Create maketable.sql

---
+++ maketable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE userProfile(
+    userId INT,
+    LastName VARCHAR(25),
+    FirstName VARCHAR(25),
+    walletId int,
+    nftId int,
+    eth DOUBLE PRECISION,
+    nftDate VARCHAR(30),
+    Email VARCHAR(30),
+    userName VARCHAR(25),
+    UserPassword VARCHAR(25),
+    r int,
+    g int,
+    b int
+);
